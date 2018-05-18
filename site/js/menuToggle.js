5701ba5f2a7f0ca3ace8c602edd2f0bdfa1b2c96
Vincent Uden <vincent.uden@itggot.se>
2018-05-18 13:54:51 +0200

    Finished inex.html and started work on docs

--- site/js/menuToggle.js
+++ site/js/menuToggle.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
     var element = document.querySelector(".menu");
     element.classList.toggle("show");
-    element = document.querySelector(".fade");
-    element.classList.toggle("showFade");
+    // element = document.querySelector(".fade");
+    // element.classList.toggle("showFade");
 }
